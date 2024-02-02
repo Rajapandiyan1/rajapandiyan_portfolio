af060cbb820486755e84c5b4fbc6202d0e45faa3
Rajapandiyan <rajapandiyan1163@gmail.com>
2024-02-02 08:34:26 +0530

    contact add placeholder add message success edit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,14 +4,29 @@ import emailjs from 'emailjs-com'
 import {motion} from 'framer-motion'
 function Contact(props) {
   let [data,setData]=useState({to_name:'',message:'',email:''});
-let [suc,setsuc]=useState(false)
-let [err,seterr]=useState(false)
+let [suc,setsuc]=useState(true)
+let [err,seterr]=useState(false);
+let [cuserr,setcus]=useState({email:false,name:false,message:false});
+let [valid,setvalid]=useState(false)
   useEffect(()=>{
     props.message("contact");
   })
  async function sub(params) {
   let clone=data;
-  
+  setvalid(false)
+  setcus({email:false,name:false,message:false})
+  if(clone.email == ''){
+    setcus((prev)=>{return {...prev,email:true}})
+  setvalid(true)
+  }
+  if(clone.to_name == ''){
+    setcus((prev)=>{return {...prev,name:true}})
+  setvalid(true)
+  }
+  if(clone.message == ''){
+    setcus((prev)=>{return {...prev,message:true}})
+  setvalid(true)
+  }
   setData({to_name:'',message:'',email:''})
    await emailjs.init('pJdjQkbV3aLx7enpa');
    await emailjs.send('service_cut4955','template_wd4krr7',clone).then((data)=>{if(data.status==200){setsuc(true)}else{
@@ -23,16 +38,17 @@ let [err,seterr]=useState(false)
    }, 5000);
   }
   return (
-    <div className='ms-0 me-0 ps-md-5 pt-md-5' style={{minHeight:'86vh',marginTop:'10vh'}}>
-      <div className="row me-0 justify-content-center align-items-center">
-{suc && <div className='col-12 alert alert-dismissible alert-success me-4'>
+    <div className='ms-0 me-0 ps-md-5 pt-md-5' style={{minHeight:'86vh',marginTop:'7vh'}}>
+      <div className="row me-0 ms-0 justify-content-center align-items-center">
+{suc && <div className='col-11 mt-4 alert alert-dismissible alert-success'>
   <div className="btn-close" onClick={()=>{setsuc(false)}}>
     </div>
     send mail successfully</div>}
-    {err && <div className='col-12 alert alert-dismissible alert-success me-4'>
+    {err && <div className='col-12 alert alert-dismissible alert-danger'>
   <div className="btn-close" onClick={()=>{seterr(false)}}>
     </div>
-    send mail successfully</div>}
+    send mail faild</div>}
+
         <div className="col-12 mt-3 mt-md-0">
         <div  className=' col-12 ps-5  h1 fw-6'>Contact Us</div>
         </div>
@@ -51,17 +67,18 @@ let [err,seterr]=useState(false)
 </div>
 </div>
       <div className="col-md-8 order-1">
-<div className="row justify-content-center ">
+<div className="row justify-content-center mb-5">
      
     <i className="fa-li fa fa-square"></i>
       <div className="col-11 form-group"><label className='form-check-label mt-3 fw-6'>Name</label></div>
       <div className="col-11"><input type="text" value={data.to_name} placeholder='Enter Your Name' onChange={(e)=>{setData((prev)=>{return {...prev,to_name:e.target.value}})}} className='form-control mt-3'/></div>
+      {cuserr.name && <label>Please enter your name</label>}
       <div className="col-11"><label className='form-check-label mt-3' htmlFor="">Email</label></div>
-      <div className="col-11"><input type="email" value={data.email} onChange={(e)=>{setData((prev)=>{return {...prev,email
+      <div className="col-11"><input type="email" placeholder='Enter your email' value={data.email} onChange={(e)=>{setData((prev)=>{return {...prev,email
         :e.target.value}})}} className='form-control mt-3' /></div>
       <div className="col-11"><label className='form-check-label mt-3' htmlFor="">Message</label></div>
       <div className="col-11"><textarea name="" placeholder='Enter the Message' value={data.message} onChange={(e)=>{setData((prev)=>{return {...prev,message:e.target.value}})}} className='form-control mt-3' id="" cols="3"></textarea></div>
-      <div className="col-11 d-flex justify-content-end"><button className="btn btn-primary mt-3" onClick={()=>{sub()}}>Submit</button></div>
+      <div className="col-11 d-flex justify-content-end "><button className="btn btn-primary mt-3" onClick={()=>{sub()}}>Submit</button></div>
  
 </div>
       </div>
