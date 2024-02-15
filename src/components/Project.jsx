da8e0916cc0336f68d33c716301dbae8051c0068
Rajapandiyan <rajapandiyan1163@gmail.com>
2024-02-15 11:13:18 +0530

    project componet e-commerce added

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -10,64 +10,54 @@ function Project(props) {
     props.message("project");
   }, [])
   return (
-    <div className='rounded-3' style={{ minHeight: '86vh', marginTop: '8vh', marginBottom: '5vh' }}>
-      <div className="row ms-0 me-0 justify-content-sm-center justify-content-md-evenly">
-        <div className="col-12 col-sm-10 col-md-4 col-lg-3 mt-5">
-          <div className="card" style={{ width: '100%', minHeight: '300px' }}>
-            <div className="card-img-top">
-              <img src={ecom} className='img-fluid w-100' style={{ height: '200px' }} alt="" />
-            </div>
-            <div className="card-body">
-              <h5 className="card-tittle">
-                E-commerce
-              </h5>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor neque maxime totam eos deleniti eligendi blanditiis, iusto praesentium aspernatur similique modi rem illo. Unde voluptatibus amet aliquid quaerat vitae velit!
-            </div>
-            <h5 className="card-footer bg-warning mb-0 text-center">
-              <a href="" className='text-decoration-none text-dark'>
-                Project Link
-              </a>
-            </h5>
-          </div>
-        </div>
-        
-        <div className="col-12 col-sm-10 col-md-4 col-lg-3 mt-5">
-          <div className="card" style={{ width: '100%', minHeight: '300px' }}>
-            <div className="card-img-top">
-              <img src={tea} className='img-fluid w-100' style={{ height: '200px' }} alt="" />
-            </div>
-            <div className="card-body">
-              <h5 className="card-tittle">
-                Tea Shop
-              </h5>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor neque maxime totam eos deleniti eligendi blanditiis, iusto praesentium aspernatur similique modi rem illo. Unde voluptatibus amet aliquid quaerat vitae velit!
-            </div>
-            <h5 className="card-footer bg-warning mb-0 text-center">
-              <a href="" className='text-decoration-none text-dark'>
-                Project Link
-              </a>
-            </h5>
-          </div>
-        </div>
-        <div className="col-12 col-sm-10 col-md-4 col-lg-3 mt-5">
-          <div className="card" style={{ width: '100%', minHeight: '300px' }}>
-            <div className="card-img-top">
-              <img src={gal} className='img-fluid w-100' style={{ height: '200px' }} alt="" />
-            </div>
-            <div className="card-body">
-              <h5 className="card-tittle">
-                Gallery
-              </h5>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor neque maxime totam eos deleniti eligendi blanditiis, iusto praesentium aspernatur similique modi rem illo. Unde voluptatibus amet aliquid quaerat vitae velit!
-            </div>
-            <h5 className="card-footer bg-warning mb-0 text-center">
-              <a href="" className='text-decoration-none text-dark'>
-                Project Link
-              </a>
-            </h5>
-          </div>
-        </div>
+    <div className='rounded-3 justify-content-center row me-0 border' style={{ minHeight: '86vh', marginTop: '8vh', marginBottom: '5vh' }}>
+     <div className="ps-5 mt-4 col-12">
+      <h2>E - COMMERCE </h2>
+     </div>
+      <div className="col-8 col-sm-4 d-flex align-items-center">
+<div className="card">
+  
+  <div className="card-img-top " >
+    <img className='w-100 ratioa' src={ecom} alt="" />
+  </div>
+</div>
       </div>
+      <div className="col-11 mt-3 mt-sm-0 col-sm-8 d-flex align-items-center">
+        <p>
+          <p>
+Description:
+Developed a feature-rich E-Commerce web application using React.js and Redux
+          </p>
+          <p>
+1. **User Authentication:**- Designed and implemented a secure user
+authentication system with login and register functionalities, ensuring a
+personalized experience for each user.
+          </p>
+          <p>    
+2. **Shopping Cart:**- Integrated an "Add to Cart" feature allowing users to easily
+add products to their shopping cart for a streamlined shopping process.
+          </p>
+          <p>
+
+3. **Order Management:**- Implemented a 'My Orders' section where users can
+track their order history, view order details, and check the status of their
+purchases.
+          </p>
+4. **Form Validation:**- Implemented robust form validation to enhance data
+accuracy and user experience during registration and checkout processes.
+
+<p>
+
+5. **Toast Messages:**- Incorporated toast messages to provide real-time
+feedback to users, enhancing the overall usability of the application.
+</p>
+        </p>
+        
+      </div>
+      <div></div>
+    <div className='d-flex justify-content-end pe-5 align-items-center'>
+        Project - Link :   <button className="btn btn-success ms-3"><a href="" className='text-white text-decoration-none'>Link</a></button>
+        </div>
     </div>
 
   )
